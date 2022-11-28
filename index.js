eba9b6213d66d0adbf19715c3183838f0e7cd435
Max Programming <ui.sabuwala@gmail.com>
2022-11-28 16:04:55 +0530

    Challenge Solved

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 // dom elements
-const buttons = document.querySelectorAll('button')
-const textarea = document.querySelector('textarea')
-const output = document.querySelector('.output')
+const buttons = document.querySelectorAll('button');
+const textarea = document.querySelector('textarea');
+const output = document.querySelector('.output');
 
 // flavours
 const flavours = {
   ninja: ['dojo', 'shogun', 'shinobi', 'samuri', 'shuriken'],
   pokemon: ['pika', 'pokeball', 'pokedex', 'evolve', 'ash', 'gym'],
   space: ['universe', 'galaxy', 'telescope', 'comet', 'stars'],
-}
+};
 
-// functions
+/**
+ * @param {string} inputText
+ * @param {string} flavour
+ */
 const flavourize = (inputText, flavour) => {
- 
-}
+  if (flavours[flavour] === undefined) return '';
+  let flavourIndex = -1;
 
-const updateOutput = (text) => {
+  const words = inputText.split(' ');
+  const flavourizedWords = words.map((word, index) => {
+    if (flavourIndex === flavours[flavour].length - 1) flavourIndex = 0;
+    else flavourIndex++;
 
-}
+    if (index === 0 || index % 3 === 0) return flavours[flavour][flavourIndex];
+    return word;
+  });
+  return flavourizedWords.join(' ');
+};
+
+const updateOutput = text => {
+  output.innerText = text;
+};
 
 // event listener
 buttons.forEach(b => {
-  b.addEventListener('click', (e) => {
-
-
-  })
-})
-
+  b.addEventListener('click', e => {
+    const flavourizedWords = flavourize(textarea.value, b.dataset.flavour);
+    updateOutput(flavourizedWords);
+  });
+});
